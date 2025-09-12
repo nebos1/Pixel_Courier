d8652141874d514a06d3d3a8fcbef89b2f13e7bc
nebos1 <niki.htodorov@gmail.com>
2025-09-13 00:42:50 +0300

    temporary for what to do next

---
+++ PixelCourier/TODO.h
@@ -0,0 +1,40 @@
+
+//
+//
+// 
+// 
+// MAIN TODO NOW: add collision logic  to vehicles
+//
+//
+//
+
+//
+// LATER TODO: GAMEOVER WITH COLLISION WITH VEHICLE
+//
+
+
+
+
+
+
+// LATER LATER TODO: add clock for time remaining to deliver the packages
+
+
+// VERY LATE TODO: add logic for how to collect and deliver packages (waiting 2 sec near the front door of the house)
+
+// you can collect 1 package, then to deliver it to other house or put it in your courier house
+// +30 sec every time you deliver a package or put it in your courier house
+// start with 3 min (180 sec) timer
+// packages randomly will spawn in front of houses (3 packages at a time)
+// no clues where the packages are, you have to find them, no minimap
+
+// vehicle hit = game over (run over) or time up = game over
+
+// 1 package delivered = +10 score
+// score is shown in the main game screen left up corner, time on right up corner like a digital rect clock
+
+// TO FIX: buildings positions + to fulfill the map
+
+// OPTIONAL: maybe add 1 dog that chases u when u are in certain area - maybe near the church right side 
+// (dog catch u - 20 points and - 20 sec as penalty) 
+
