0e745fe45f5e4a1646fe4432a63628c41725d2da
nebos1 <https://github.com/nebos1>
2026-01-14 22:58:45 +0200

    ---

--- PixelCourier/TODO.h
+++ PixelCourier/TODO.h
@@ -1,32 +1,12 @@
+// TODO:
 
-
+// -> ADD:
+// sound effects on game start, on game end, on collecting package, on delivering package
+// add background music loop
+// add fence + dog near the church
 //
-// MAIN TODO: GAMEOVER WITH COLLISION WITH VEHICLE AND GAMEOVER SCREEN
+// -> IMPROVE:
+// courier positioning (idling and last position known facing)
 //
-
-
-
-
-
-
-// LATER LATER TODO: add clock for time remaining to deliver the packages
-
-
-// VERY LATE TODO: add logic for how to collect and deliver packages (waiting 2 sec near the front door of the house)
-
-// you can collect 1 package, then to deliver it to other house or put it in your courier house
-// +30 sec every time you deliver a package or put it in your courier house
-// start with 3 min (180 sec) timer
-// packages randomly will spawn in front of houses (3 packages at a time)
-// no clues where the packages are, you have to find them, no minimap
-
-// vehicle hit = game over (run over) or time up = game over
-
-// 1 package delivered = +10 score
-// score is shown in the main game screen left up corner, time on right up corner like a digital rect clock
-
-// TO FIX: buildings positions + to fulfill the map
-
-// OPTIONAL: maybe add 1 dog that chases u when u are in certain area - maybe near the church right side 
-// (dog catch u - 20 points and - 20 sec as penalty) 
-
+// -> FIX:
+// current building positions and add the rest
