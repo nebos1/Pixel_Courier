e8f97cc084337991ec9adaf7a291a0e9fd0e597a
Nikolay Todorov <niki.htodorov@gmail.com>
2026-02-23 17:10:24 +0200

    refactoring function for package delivery logic

--- PixelCourier/Package_delivery.h
+++ PixelCourier/Package_delivery.h
@@ -1,3 +1,5 @@
+// header for package delivery logic
+
 #pragma once
 
 #include <SFML/Graphics.hpp>
@@ -11,54 +13,67 @@
 
 
 class PackageDeliverySystem {
-public:
-    PackageDeliverySystem() {
-        SetupDropZone(drop_zone);
+private:
+    // delivery zone around courier_house
+    sf::RectangleShape drop_zone;
+    bool inside_drop_zone = false;
+    std::chrono::steady_clock::time_point in_zone_to_deliver_package; // for package drop 
+
+    void PositionDropZone(const Sprites& sprites) {
+        if (sprites.courier_house.empty()) return;
+
+        const sf::Sprite& courier_house = sprites.courier_house[0];
+        const sf::FloatRect bounds_courier_house = courier_house.getGlobalBounds();
+
+        const float scale_zone = 1.0f; // scale the zone depending on how big courier_house bounds are
+
+        sf::Vector2f size(bounds_courier_house.width * scale_zone, bounds_courier_house.height * scale_zone); // the size
+        drop_zone.setSize(size);
+        drop_zone.setOrigin(size.x, size.y);
+        
+        // centre the zone
+        drop_zone.setPosition(bounds_courier_house.left + bounds_courier_house.width, bounds_courier_house.top + bounds_courier_house.height);
     }
 
-    void Update(
-        float delta_time,
-        const sf::FloatRect& courier_box,
-        const Sprites& sprites,
-        bool& package_collected,
-        int& score
-    ) {
-        UpdateDropZonePosition(sprites);
+public:
+    PackageDeliverySystem() {}
 
+    void Update(float delta_time, const sf::FloatRect& courier_box, const Sprites& sprites, bool& package_collected, bool& package_delivered, int& score) {
+        PositionDropZone(sprites);
+        package_delivered = false;
+        // if not carrying a package and trying to deliver (empty thing) or at all not carrying a package => no holding
         if (!package_collected) {
-            ResetHold();
+            inside_drop_zone = false;
             return;
         }
 
         const auto now = std::chrono::steady_clock::now();
         const sf::FloatRect zone = drop_zone.getGlobalBounds();
 
-        if (!zone.intersects(courier_box)) {
-            ResetHold();
-            return;
+        // if in the zone holding a package and inside drop_zone
+        if (zone.intersects(courier_box)) {
+            // check if courier inside zone and start countdown
+            if (inside_drop_zone == false) {
+                inside_drop_zone = true;
+                in_zone_to_deliver_package = now;
+                return;
+            }
+            // check the countdown if 2 sec passed 
+            if (now - in_zone_to_deliver_package >= Timers_OBJ.courier_drop_hold_time) {
+                // delivered
+                package_collected = false;
+                package_delivered = true;
+
+                // bonus time 
+                ClockDisplay_OBJ.AddTime(Timers_OBJ.package_delivered);
+
+                // score
+                score += 1;
+            }
         }
-
-        if (!holding) {
-            holding = true;
-            hold_start = now;
-            return;
-        }
-
-        if (now - hold_start >= Timers_OBJ.courier_drop_hold_time) {
-            // delivered
-            package_collected = false;
-
-            // bonus time
-            ClockDisplay_OBJ.AddTime(Timers_OBJ.package_delivered);
-
-            // score
-            score += 1;
-            const sf::Vector2u window_size(1280, 720);
-            sf::RenderWindow window(sf::VideoMode(window_size.x, window_size.y), "Game");
-
-            ScoreDisplay_OBJ.SetScore(score, window_size);
-
-            ResetHold();
+        else {
+            // outside zone -> reset holding state
+            inside_drop_zone = false;
         }
     }
 
@@ -67,32 +82,4 @@ public:
         target.draw(drop_zone);
     }
 
-private:
-    // delivery zone around courier_house
-    sf::RectangleShape drop_zone;
-
-    bool holding = false;
-    std::chrono::steady_clock::time_point hold_start{};
-
-private:
-    static void SetupDropZone(sf::RectangleShape& zone) {
-        zone.setSize(sf::Vector2f(100.f, 100.f));
-        zone.setOrigin(zone.getSize().x / 2.f, zone.getSize().y / 2.f);
-
-        // TEST ONLY
-       // zone.setFillColor(sf::Color(200, 160, 0, 80));
-       // zone.setOutlineThickness(2.f);
-       // zone.setOutlineColor(sf::Color(200, 160, 0, 220));
-    }
-
-    void UpdateDropZonePosition(const Sprites& sprites) {
-        if (!sprites.courier_house.empty()) {
-            // center zone on courier_house sprite position
-            drop_zone.setPosition(sprites.courier_house[0].getPosition() + sf::Vector2f(50.0f, 50.0f));
-        }
-    }
-
-    void ResetHold() {
-        holding = false;
-    }
 };
